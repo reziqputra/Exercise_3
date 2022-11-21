7bc13fe3333fac74c0a480808dae2a840ee88220
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-11-21 17:17:03 +0800

    create case 2

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -81,6 +81,27 @@
                                 obj.traverse();
                             }
                             break;
+                        case '2':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.Write("\nEnter the roll number of the student whose record is to be searched: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number: " + curr.rollNumber);
+                                    Console.WriteLine("\nName: " + curr.name);
+                                }
+                            }
+                            break;
                     }
                 }
             }
