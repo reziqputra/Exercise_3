c1b857709ad678f8810a74cb75a0d7de6a401cff
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-11-21 17:04:42 +0800

    create function if else in public void firstNode

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -53,5 +53,12 @@
                 Console.Write(LAST.rollNumber + "       " + LAST.name + "\n");
             }
         }
+        public void firstNode()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+                Console.WriteLine("\nThe first record in the list is:\n\n " + LAST.next.rollNumber + "      " + LAST.next.name);
+        }
     }
 }
