87257dd284646c0dccd244c121a0602364d5fdf5
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-11-21 16:52:29 +0800

    create function in public bool Search

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -15,6 +15,18 @@
         {
             LAST = null;
         }
-
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+            /*Searches for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return true;/*return true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber)/*if the node is present at the end*/
+                return true;
+            else
+                return (false);/*returns false if the node is not found*/
+        }
     }
 }
