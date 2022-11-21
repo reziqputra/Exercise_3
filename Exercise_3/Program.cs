57d12635b2fc9011936221c0cd6e4ea870f0e2fb
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-11-21 17:10:38 +0800

    create case 1

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -60,5 +60,30 @@
             else
                 Console.WriteLine("\nThe first record in the list is:\n\n " + LAST.next.rollNumber + "      " + LAST.next.name);
         }
+        static void Main(string[] args)
+        {
+            CircularList obj = new CircularList();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. View all the records in the list");
+                    Console.WriteLine("2. Search for a record in the list");
+                    Console.WriteLine("3. Display the first record in the list");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("\nEnter your choice (1-4): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.traverse();
+                            }
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
